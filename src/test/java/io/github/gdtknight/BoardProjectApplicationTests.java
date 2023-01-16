a96dc9df175903d2d184af68b98c1d24c2aaaf00
gdtknight <gdtknight@naver.com>
2023-01-16 11:32:38 +0900

    #1 - Project initialization
    Spring Boot 활용해서 프로젝트 기본 생성

---
+++ src/test/java/io/github/gdtknight/BoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gdtknight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
