34e6a4a950cfc6ff9e083ad9e34283724a1d41ca
gdtknight <gdtknight@naver.com>
2023-01-18 07:35:58 +0900

    #7 - Article, ArticleComment 각각의 Repository CRUD Test 를 JpaRepositoryTest Class 에서 수행

---
+++ src/test/java/io/github/gdtknight/repository/JpaRepositoryTest.java
@@ -0,0 +1,35 @@
+package io.github.gdtknight.repository;
+
+// import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import io.github.gdtknight.config.JpaConfig;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+public class JpaRepositoryTest {
+  private final ArticleRepository articleRepository;
+  private final ArticleCommentRepository articleCommentRepository;
+
+  public JpaRepositoryTest(
+      @Autowired ArticleRepository articleRepository,
+      @Autowired ArticleCommentRepository articleCommentRepository) {
+    this.articleRepository = articleRepository;
+    this.articleCommentRepository = articleCommentRepository;
+  }
+
+  @DisplayName("select 테스트")
+  @Test
+  void givenTestData_whenSelecting_thenWorksFine() {
+    // given
+    // when
+    // then
+
+  }
+}
