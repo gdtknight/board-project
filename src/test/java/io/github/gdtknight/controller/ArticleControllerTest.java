0419590ffc33819abdbcc044d8624b683f08513c
gdtknight <gdtknight@naver.com>
2023-02-08 11:11:41 +0900

    #29 - modify ArticleControllerTest
    검색 기능 관련 구현을 위해 테스트 먼저 수정

--- src/test/java/io/github/gdtknight/controller/ArticleControllerTest.java
+++ src/test/java/io/github/gdtknight/controller/ArticleControllerTest.java
@@ -1,28 +1,46 @@
 package io.github.gdtknight.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import io.github.gdtknight.config.SecurityConfig;
+import io.github.gdtknight.config.JpaConfig;
+import io.github.gdtknight.dto.ArticleWithCommentsDto;
+import io.github.gdtknight.dto.UserAccountDto;
+import io.github.gdtknight.service.ArticleService;
 
 @DisplayName("View 컨트롤러 - 게시글")
-@Import(SecurityConfig.class)
+@Import({ SecurityConfig.class })
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
   private final MockMvc mvc;
+
+  // 필드 인젝션은 권장되지 않음
+  @MockBean
+  private ArticleService articleService;
 
   public ArticleControllerTest(@Autowired MockMvc mvc) {
     this.mvc = mvc;
@@ -32,6 +50,7 @@ class ArticleControllerTest {
   @Test
   public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
     // Given
+    given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
 
     // When
 
@@ -40,23 +59,28 @@ class ArticleControllerTest {
         .andExpect(status().isOk())
         .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
         .andExpect(view().name("articles/index"))
-        .andExpect(model().attributeExists("articles"));
+        .andExpect(model().attributeExists("articles"))
+        .andExpect(model().attributeExists("searchTypes"));
+    then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
   }
 
   @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
   @Test
   public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
     // Given
+    Long articleId = 1L;
+    given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
     // When
 
     // Then
-    mvc.perform(get("/articles/1"))
+    mvc.perform(get("/articles/" + articleId))
         .andExpect(status().isOk())
         .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
         .andExpect(view().name("articles/detail"))
         .andExpect(model().attributeExists("article"))
         .andExpect(model().attributeExists("articleComments"));
+    then(articleService).should().getArticle(articleId);
   }
 
   @Disabled("구현중")
@@ -88,5 +112,33 @@ class ArticleControllerTest {
         .andExpect(status().isOk())
         .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
         .andExpect(view().name("articles/search-hashtag"));
+  }
+
+  private ArticleWithCommentsDto createArticleWithCommentsDto() {
+    return ArticleWithCommentsDto.of(
+        1L,
+        createUserAccountDto(),
+        Set.of(),
+        "title",
+        "content",
+        "#java",
+        LocalDateTime.now(),
+        "uno",
+        LocalDateTime.now(),
+        "uno");
+  }
+
+  private UserAccountDto createUserAccountDto() {
+    return UserAccountDto.of(
+        1L,
+        "uno",
+        "pw",
+        "uno@gmail.com",
+        "Uno",
+        "memo",
+        LocalDateTime.now(),
+        "uno",
+        LocalDateTime.now(),
+        "uno");
   }
 }
