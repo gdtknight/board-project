8365af56336b11312c428e89e136671bf50ba038
gdtknight <gdtknight@naver.com>
2023-02-27 23:53:54 +0900

    #40 - modify ArticleController.java

--- src/main/java/io/github/gdtknight/controller/ArticleController.java
+++ src/main/java/io/github/gdtknight/controller/ArticleController.java
@@ -44,6 +44,8 @@ public class ArticleController {
             articles.getTotalPages());
     map.addAttribute("articles", articles);
     map.addAttribute("paginationBarNumbers", barNumbers);
+    map.addAttribute("searchTypes", searchType.values());
+
     return "articles/index";
 
   }
