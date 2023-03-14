d6f4d11cb8b9c887f1c212a63441dd33875ddc17
gdtknight <gdtknight@naver.com>
2023-03-14 16:08:32 +0900

    #45 - feat. 게시글 등록 삭제 기능 추가

--- src/main/java/io/github/gdtknight/controller/ArticleController.java
+++ src/main/java/io/github/gdtknight/controller/ArticleController.java
@@ -10,12 +10,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import io.github.gdtknight.domain.type.SearchType;
+import io.github.gdtknight.domain.constant.FormStatus;
+import io.github.gdtknight.domain.constant.SearchType;
 import io.github.gdtknight.dto.ArticleResponse;
 import io.github.gdtknight.dto.ArticleWithCommentsResponse;
+import io.github.gdtknight.dto.UserAccountDto;
+import io.github.gdtknight.dto.request.ArticleRequest;
 import io.github.gdtknight.service.ArticleService;
 import io.github.gdtknight.service.PaginationService;
 import lombok.RequiredArgsConstructor;
@@ -81,5 +85,48 @@ public class ArticleController {
     map.addAttribute("searchType", SearchType.HASHTAG);
 
     return "articles/search-hashtag";
+  }
+
+  @GetMapping("/form")
+  public String articleForm(ModelMap map) {
+    map.addAttribute("formStatus", FormStatus.CREATE);
+
+    return "articles/form";
+  }
+
+  @PostMapping("/form")
+  public String postNewArticle(ArticleRequest articleRequest) {
+    // TODO: 인증 정보를 넣어줘야 한다.
+    articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
+        "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null)));
+
+    return "redirect:/articles";
+  }
+
+  @GetMapping("/{articleId}/form")
+  public String updateArticleForm(@PathVariable Long articleId, ModelMap map) {
+    ArticleResponse article = ArticleResponse.fromArticleDto(articleService.getArticle(articleId));
+
+    map.addAttribute("article", article);
+    map.addAttribute("formStatus", FormStatus.UPDATE);
+
+    return "articles/form";
+  }
+
+  @PostMapping("/{articleId}/form")
+  public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
+    // TODO: 인증 정보를 넣어줘야 한다.
+    articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
+        "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null)));
+
+    return "redirect:/articles/" + articleId;
+  }
+
+  @PostMapping("/{articleId}/delete")
+  public String deleteArticle(@PathVariable Long articleId) {
+    // TODO: 인증 정보를 넣어줘야 한다.
+    articleService.deleteArticle(articleId);
+
+    return "redirect:/articles";
   }
 }
