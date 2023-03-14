829102dea56f857c9d110a433cdb3af71a9008dd
gdtknight <gdtknight@naver.com>
2023-03-14 17:39:57 +0900

    #45 - 댓글 등록, 삭제 요청에 사용되는 Dto class

---
+++ src/main/java/io/github/gdtknight/dto/request/ArticleCommentRequest.java
@@ -0,0 +1,19 @@
+package io.github.gdtknight.dto.request;
+
+import io.github.gdtknight.dto.ArticleCommentDto;
+import io.github.gdtknight.dto.UserAccountDto;
+
+public record ArticleCommentRequest(Long articleId, String content) {
+
+  public static ArticleCommentRequest of(Long articleId, String content) {
+    return new ArticleCommentRequest(articleId, content);
+  }
+
+  public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
+    return ArticleCommentDto.of(
+        articleId,
+        userAccountDto,
+        content);
+  }
+
+}
