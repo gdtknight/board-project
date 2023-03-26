dab6b3a3cac38d23cc77959f864acbb8dfdd5d29
yongho shin <gdtknight@naver.com>
2023-03-26 21:20:28 +0900

    #47- Controller, Service Layer에 Spring Security 적용
    Spring Security 를 적용하여 인증된 이용자만 게시글 등록, 삭제, 수정 및 댓글 등록, 삭제가 가능하도록 구현하였다. 인증된 사용자와 관련된 정보는 BoardPrincipal 을 통해 접근이 가능하다.

--- src/main/java/io/github/gdtknight/controller/ArticleCommentController.java
+++ src/main/java/io/github/gdtknight/controller/ArticleCommentController.java
@@ -1,5 +1,7 @@
 package io.github.gdtknight.controller;
 
+import io.github.gdtknight.security.BoardPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,17 +20,22 @@ public class ArticleCommentController {
   private final ArticleCommentService articleCommentService;
 
   @PostMapping("/new")
-  public String postNewArticleComment(ArticleCommentRequest articleCommentRequest) {
-    // TODO: 인증 정보를 넣어줘야 한다.
-    articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
-        "uno", "pw", "uno@mail.com", null, null)));
+  public String postNewArticleComment(
+          @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+          ArticleCommentRequest articleCommentRequest
+  ) {
+    articleCommentService.saveArticleComment(articleCommentRequest.toDto(boardPrincipal.toDto()));
 
     return "redirect:/articles/" + articleCommentRequest.articleId();
   }
 
   @PostMapping("/{commentId}/delete")
-  public String deleteArticleComment(@PathVariable Long commentId, Long articleId) {
-    articleCommentService.deleteArticleComment(commentId);
+  public String deleteArticleComment(
+          @PathVariable Long commentId,
+          @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+          Long articleId
+  ) {
+    articleCommentService.deleteArticleComment(commentId,boardPrincipal.getUsername());
 
     return "redirect:/articles/" + articleId;
   }
--- src/main/java/io/github/gdtknight/controller/ArticleController.java
+++ src/main/java/io/github/gdtknight/controller/ArticleController.java
@@ -2,10 +2,14 @@ package io.github.gdtknight.controller;
 
 import java.util.List;
 
+import io.github.gdtknight.security.BoardPrincipal;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -96,10 +100,11 @@ public class ArticleController {
   }
 
   @PostMapping("/form")
-  public String postNewArticle(ArticleRequest articleRequest) {
-    // TODO: 인증 정보를 넣어줘야 한다.
-    articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-        "uno", "asdf1234", "uno@mail.com", "Uno", "memo")));
+  public String postNewArticle(
+          @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+          ArticleRequest articleRequest
+  ) {
+    articleService.saveArticle(articleRequest.toDto(boardPrincipal.toDto()));
     return "redirect:/articles";
   }
 
@@ -114,18 +119,23 @@ public class ArticleController {
   }
 
   @PostMapping("/{articleId}/form")
-  public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
-    // TODO: 인증 정보를 넣어줘야 한다.
-    articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-        "uno", "asdf1234", "uno@mail.com", "Uno", "memo")));
+  public String updateArticle(
+          @PathVariable Long articleId,
+          @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+          ArticleRequest articleRequest
+  ) {
+
+    articleService.updateArticle(articleId, articleRequest.toDto(boardPrincipal.toDto()));
 
     return "redirect:/articles/" + articleId;
   }
 
   @PostMapping("/{articleId}/delete")
-  public String deleteArticle(@PathVariable Long articleId) {
-    // TODO: 인증 정보를 넣어줘야 한다.
-    articleService.deleteArticle(articleId);
+  public String deleteArticle(
+          @PathVariable Long articleId,
+          @AuthenticationPrincipal BoardPrincipal boardPrincipal
+          ) {
+    articleService.deleteArticle(articleId, boardPrincipal.getUsername());
 
     return "redirect:/articles";
   }
